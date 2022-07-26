2f7b9e56ddb16ad46203d60a77c69e6ccbdfc427
GAutuori94 <101925081+GAutuori94@users.noreply.github.com>
2022-07-26 11:57:30 +0200

    mod: added conditional rendering to Welcome component

--- src/hello.js
+++ src/hello.js
@@ -2,19 +2,24 @@ import React from "react";
 
 export class Hello extends React.Component {
   render() {
-    return <h1>Hello World</h1>
+    return <h1>Hello World</h1>;
   }
 }
 
-
 export class Message extends React.Component {
-    render () {
-        return <p> What a beautiful day!</p>
-    }
+  render() {
+    return <p> What a beautiful day!</p>;
+  }
 }
 
 export class Welcome extends React.Component {
-    render () {
-        return <p> Welcome {this.props.name} </p>
-    }
+  render() {
+    return (
+      <div>
+        {this.props.name 
+        ? <p> Welcome {this.props.name} </p>
+        : <p> Welcome, new user </p>}
+      </div>
+    )
+  }
 }
