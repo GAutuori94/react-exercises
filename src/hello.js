51acbb30058a6704a006259c70d761231e175d91
GAutuori94 <101925081+GAutuori94@users.noreply.github.com>
2022-07-26 19:15:24 +0200

    correzione da conditional rendering a defaultprops in Welcome

--- src/hello.js
+++ src/hello.js
@@ -2,24 +2,23 @@ import React from "react";
 
 export class Hello extends React.Component {
   render() {
-    return <h1>Hello World</h1>;
+    return <h1>Hello World</h1>
   }
 }
 
+
 export class Message extends React.Component {
-  render() {
-    return <p> What a beautiful day!</p>;
-  }
+    render () {
+        return <p> What a beautiful day!</p>
+    }
 }
 
 export class Welcome extends React.Component {
-  render() {
-    return (
-      <div>
-        {this.props.name 
-        ? <p> Welcome {this.props.name} </p>
-        : <p> Welcome, new user </p>}
-      </div>
-    )
-  }
+    render () {
+        return <p> Welcome {this.props.name} </p>
+    }
+}
+
+Welcome.defaultProps = {
+    name: "Giorgio"
 }
