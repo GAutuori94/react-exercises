9b3509c4f09fb85a8ad28f52f60604b3d90f84c6
GAutuori94 <101925081+GAutuori94@users.noreply.github.com>
2022-08-04 20:26:09 +0200

    added LanguageContext consumer in DisplayLanguage component

--- src/displayLanguage.js
+++ src/displayLanguage.js
@@ -1,23 +1,23 @@
 import React from "react";
 import { LanguageContext } from "./languageContext";
 
-// const Strings = {
-// en: {
-//         CURRENT_LANGUAGE: "The selected language is: "
-//     },
-// it: {
-//         CURRENT_LANGUAGE: "La lingua selezionata è: "
-//     }
-// }
+const Strings = {
+en: {
+        CURRENT_LANGUAGE: "The selected language is: "
+    },
+it: {
+        CURRENT_LANGUAGE: "La lingua selezionata è: "
+    }
+}
 
 export class DisplayLanguage extends React.Component {
     render () {
         return <div>
-            {/* <LanguageContext.Consumer>
+            <LanguageContext.Consumer>
                 {(language)  => {
                     return <h1> {Strings[language].CURRENT_LANGUAGE} {language}</h1>
                 }}
-            </LanguageContext.Consumer> */}
+            </LanguageContext.Consumer>
         </div>
     }
 }
