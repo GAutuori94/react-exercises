f8d015ffc85af54a83a8f001326455f4bc02e54e
GAutuori94 <101925081+GAutuori94@users.noreply.github.com>
2022-08-05 17:14:43 +0200

    Welcome function component added to hello.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import React from "react";
-import { DisplayLanguage } from "./displayLanguage";
-import { LanguageContext } from "./languageContext";
+import { Welcome } from "./hello";
 
 export default class App extends React.Component {
-  state = {
-    language: "en",
-  };
-
-  handleLanguageChange = (event) => {
-    this.setState({
-      language: event.target.value,
-    });
-  };
+ 
   render() {
     return (
-        <LanguageContext.Provider value={this.state.language}>
-          <select
-            value={this.state.language}
-            onChange={this.handleLanguageChange}
-          >
-            <option value="en">English</option>
-            <option value="it">Italiano</option>
-          </select>
-          <DisplayLanguage />
-        </LanguageContext.Provider>  
+        <Welcome name="Jimmy"/>  
     );
   }
 }
