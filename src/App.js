dadd3b404c1d064560268c126967fc9cd756952c
GAutuori94 <101925081+GAutuori94@users.noreply.github.com>
2022-08-05 18:12:15 +0200

    ClickCounter function component with useState hook added to counter.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { Sum } from "./calculator";
+import { ClickCounter } from "./counter";
 
 export default class App extends React.Component {
  
   render() {
     return (
-        <Sum numbers={[1,2,3,4,5,6,7,8,9]}/>  
+        <ClickCounter initialValue={0} />
     );
   }
 }
