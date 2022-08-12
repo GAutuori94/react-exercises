336db94b91806f2d40f9e5b3d72f32d86f037f6a
GAutuori94 <101925081+GAutuori94@users.noreply.github.com>
2022-08-12 17:23:06 +0200

    DisplayLanguage component created, access to LangueContext through useContext hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import { CarDetails } from "./form.js";
+import { useState } from "react";
+import { DisplayLanguage } from "./displayLanguage";
+import { LanguageContext } from "./languageContext";
 
 export function App() {
 
+  const [language, setLanguage] = useState('en')
+
+  function handleChangeLanguage (event) {
+    setLanguage(event.target.value) 
+  }
+
   return (
     <div>
-      <CarDetails defaultData={{defaultModel: "Toyota", defaultYear: 2000, defaultColor: "grey"}} />
+      <select value={language} onChange={handleChangeLanguage}>
+        <option value='en'>English</option>
+        <option value='it'>Italiano</option>
+      </select>
+      <LanguageContext.Provider value={language}>
+      <DisplayLanguage />
+      </LanguageContext.Provider>
     </div>
   )
 }
