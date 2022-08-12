46538988d57239105cfe40f30822f16576e311e3
GAutuori94 <101925081+GAutuori94@users.noreply.github.com>
2022-08-12 18:32:20 +0200

    Route for Welcome component added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import { useState } from "react";
-import { DisplayLanguage } from "./displayLanguage";
-import { LanguageContext } from "./languageContext";
+import { Routes } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+import { Welcome } from './hello'
 
 export function App() {
 
-  const [language, setLanguage] = useState('en')
-
-  function handleChangeLanguage (event) {
-    setLanguage(event.target.value) 
-  }
-
   return (
     <div>
-      <select value={language} onChange={handleChangeLanguage}>
-        <option value='en'>English</option>
-        <option value='it'>Italiano</option>
-      </select>
-      <LanguageContext.Provider value={language}>
-      <DisplayLanguage />
-      </LanguageContext.Provider>
+      <Routes>
+        <Route path='/' element={<Welcome name='Giorgio' />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/hello.js
@@ -0,0 +1,7 @@
+export function Welcome({name = "World"}) {
+    return (
+      <div>
+        <h2> Hello, {name}!</h2> 
+      </div>
+    );
+  }
