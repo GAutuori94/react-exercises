8173867ce24acdeede846e3f185a0f725798f698
GAutuori94 <101925081+GAutuori94@users.noreply.github.com>
2022-08-14 19:16:11 +0200

    Page not found route added

--- src/App.js
+++ src/App.js
@@ -12,6 +12,10 @@ export function App() {
         <Route path='/' element={<Welcome name='Giorgio' />} />
         <Route path='/counter' element={<ClickCounter />} />
         <Route path='/users/:username' element={<ShowGithubUser />} />
+        <Route path='*' element={<div>
+          <h2>Page not found</h2>
+          <Link to='/'>Return to Home</Link>
+        </div>} />
       </Routes>
 
       <Link to="/">Home</Link> | <Link to="/counter">Counter</Link> | <Link to="/user/:username">Github User</Link>
